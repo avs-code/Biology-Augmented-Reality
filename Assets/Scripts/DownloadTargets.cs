4271dfef3c7a9eccb621c5146b561994be1ec0b4
Toni Vicente <arv187@users.noreply.github.com>
2023-07-02 19:18:51 +0200

    1.0.5
    Added button with targets pdf link

---
+++ Assets/Scripts/DownloadTargets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownloadTargets : MonoBehaviour
+{
+    public void Download()
+    {
+        Application.OpenURL("https://robotitostudios.files.wordpress.com/2022/08/targets-mastcell-1.pdf");
+    }
+}
