79fd88e07c9196332bec4f6b8d486cc578cc7025
Toni Vicente <sparkticus20@hotmail.com>
2024-10-17 00:50:00 +0200

    1.0.7

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public static ExitButton Instance { get; private set; }
+
+    [SerializeField] GameObject exitButton;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowExitButton()
+    {
+        exitButton.SetActive(true);
+    }
+
+    // Método para salir de la aplicación    
+    public static void MoveAndroidApplicationToBack()
+    {
+        AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        activity.Call<bool>("moveTaskToBack", true);
+    }
+}
--- Assets/Scripts/FamilyPolicyWarning.cs
+++ Assets/Scripts/FamilyPolicyWarning.cs
@@ -6,8 +6,11 @@ public class FamilyPolicyWarning : MonoBehaviour
 {
     [SerializeField] Canvas policyMessage;
 
- public void OkButton()
+    public void OkButton()
     {
+        // Desactiva el mensaje de advertencia PolicyMessage
         policyMessage.enabled = false;
+        // Llama al método para activar el botón de salida
+        ExitButton.Instance.ShowExitButton();
     }
 }
